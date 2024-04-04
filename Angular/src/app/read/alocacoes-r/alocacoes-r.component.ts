5e2f81a4979b732c33f5f7edf5e22dfe581ccf16
lalasp18 <laspio18@gmail.com>
2024-04-04 16:36:32 -0300

    tela de alocacao sem erro no cadastro

--- Angular/src/app/read/alocacoes-r/alocacoes-r.component.ts
+++ Angular/src/app/read/alocacoes-r/alocacoes-r.component.ts
@@ -381,9 +381,9 @@ export class AlocacoesRComponent implements OnInit, OnDestroy {
 
     this.multiselectEdit.writeValue(value.alunos);
     this.selectedAlunos = value.alunos;
-    value.alunos.forEach(aln => {
-      this.addAluno(aln);
-    })
+    // value.alunos.forEach(aln => {
+    //   this.addAluno(aln);
+    // })
     
     const writeEdit = this.opcaoSemana.find(ops => ops.nome == value.diaSemana);
     if (writeEdit) {
@@ -399,7 +399,6 @@ export class AlocacoesRComponent implements OnInit, OnDestroy {
     this.cadastrar = true;
     this.editar = false;
     this.switch.writeValue(null);
-    this.multiselect.writeValue(null);
     this.getAluno().clear();
     this.selectedAlunos = [];
   }
@@ -820,6 +819,8 @@ export class AlocacoesRComponent implements OnInit, OnDestroy {
   }
 
   onSubmit() {
+      console.log(this.form.value)
+
     if (this.form.valid && this.cadastrar && this.valor) {
       this.conditionCreateSave();
       this.mss = false;
@@ -851,13 +852,10 @@ export class AlocacoesRComponent implements OnInit, OnDestroy {
       //  DATAS INTERVALO
       // this.formatarDtIntervalo();
       this.diasIntervalo.forEach((dt: Date) => {
-        console.log('dt ini ', dt?.getTime() != ini?.getTime())
-        console.log('dt fim ', dt?.getTime() != this.dataFim?.getTime())
         if(dt?.getTime() != ini?.getTime() && dt?.getTime() != this.dataFim?.getTime()) {
           this.form.patchValue({
             dataAula: dt
           });
-          console.log('intv form ',this.form.value)
           this.alocacoesCadast = this.form.value;
           this.enviarFormSave();
         }
